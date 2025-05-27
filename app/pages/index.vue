8b90be416c1298224850ea30c71d3aed71e5f2e3
damnthonyy <mahassadijoseph@gmail.com>
2025-05-27 19:46:04 +0200

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="h-[500px] w-[1200px] m-[50px] bg-neutral-800 flex flex-col gap-[20px] justify-center items-center">
+
+    <div v-gsap.from="{
+    x:100,
+    duration:3,
+    ease:'power2.inOut',
+}" class="h-[50px] w-[50px] bg-blue-600 rounded-[10px]"></div>
+
+    <div v-gsap.from="{
+    x:150,
+    duration:3,
+    ease:'power2.inOut',
+}", v-gsap.to="{x:300,duration:3,ease:'power2.inOut'}" class="h-[50px] w-[50px] bg-red-600 rounded-[10px]"></div>
+
+    <div class="h-[50px] w-[50px] bg-green-600 rounded-[10px]"
+    v-gsap.whenVisible.once.from="{opacity:0,x:-100}"
+    ></div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
